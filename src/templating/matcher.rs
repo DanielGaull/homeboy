4b0dde094cb614e86cbf0b15ba329367f0275043
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-02-22 23:18:17 -0500

    Added matching strings to templates

--- src/templating/matcher.rs
+++ src/templating/matcher.rs
@@ -1,13 +1,18 @@
 use std::{collections::HashMap, error::Error};
 
+use regex::Regex;
 use thiserror::Error;
 
 use super::template::{SymbolInternal, Template};
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum TemplateError {
     #[error("Subtemplate \"{0}\" not found")]
     SubtemplateNotFound(String),
+    #[error("Template did not generate a match (regex '{0}' on input '{1}')")]
+    NoMatchForTemplate(String, String),
+    #[error("Template generated invalid regex")]
+    InvalidRegex,
 }
 
 pub struct TemplateMatcher {
@@ -25,9 +30,24 @@ impl TemplateMatcher {
         self.subtemplate_definitions.insert(String::from(name), template);
     }
 
-    pub fn try_match(&self, input: String, template: Template) -> Option<Match> {
+    pub fn try_match(&self, input: &str, template: &Template) -> Result<Match, TemplateError> {
+        let regex_str = self.convert_template_to_regex(template)?;
+        let re = Regex::new(&regex_str).map_err(|_e| TemplateError::InvalidRegex)?;
+        if let Some(captures) = re.captures(input) {
+            let named_values: HashMap<String, String> = re
+                .capture_names()
+                .flatten()
+                .filter_map(|name| captures.name(name).map(|m| (name.to_string(), m.as_str().trim().to_string())))
+                .collect();
 
-        None
+            Ok(
+                Match {
+                    variable_bindings: named_values,
+                }
+            )
+        } else {
+            Err(TemplateError::NoMatchForTemplate(regex_str, String::from(input)))
+        }
     }
 
     pub fn convert_template_to_regex(&self, template: &Template) -> Result<String, TemplateError> {
@@ -59,15 +79,19 @@ impl TemplateMatcher {
             }).collect::<Result<Vec<String>, TemplateError>>()?;
             Ok(joint_symbols.join(" "))
         }).collect::<Result<Vec<String>, TemplateError>>()?;
-        Ok(joint_clauses.join("|"))
+        let re = joint_clauses.join("|").replace(" ", r"\s*").to_lowercase();
+        Ok(re)
     }
 }
 
-struct Match {
+pub struct Match {
     variable_bindings: HashMap<String, String>,
 }
 impl Match {
-    pub fn get_binding(&self, name: &String) -> Option<&String> {
+    pub fn get_binding(&self, name: &str) -> Option<&String> {
         self.variable_bindings.get(name)
+    }
+    pub fn num_bindings(&self) -> usize {
+        self.variable_bindings.len()
     }
 }
--- tests/matching_tests.rs
+++ tests/matching_tests.rs
@@ -1,22 +1,37 @@
 use std::error::Error;
 
-use homeboy::templating::{matcher::TemplateMatcher, parser::TemplateParser};
+use homeboy::templating::{matcher::{TemplateError, TemplateMatcher}, parser::TemplateParser};
 
 #[test]
 fn regex_generation_tests() -> Result<(), Box<dyn Error>> {
-    let mut matcher = TemplateMatcher::new();
-
-    let pre_command_ask = TemplateParser::parse_template("(could|would) you please?")?;
-    matcher.add_subtemplate("pre command ask", pre_command_ask);
+    let matcher = setup_matcher()?;
 
     assert_regex("foo", "foo", &matcher)?;
     assert_regex("foo?", "foo?", &matcher)?;
     assert_regex("(foo)?", "(?:foo)?", &matcher)?;
     assert_regex("[hello]", "(?<hello>.*)", &matcher)?;
-    assert_regex("{pre command ask}?", "(?:(?:could|would) you please?)?", &matcher)?;
+    assert_regex("{pre command ask}?", r"(?:(?:could|would)\s*you\s*please?)?", &matcher)?;
     assert_regex(
         "{pre command ask}? play [song] on Spotify", 
-        "(?:(?:could|would) you please?)? play (?<song>.*) on Spotify", 
+        r"(?:(?:could|would)\s*you\s*please?)?\s*play\s*(?<song>.*)\s*on\s*spotify", 
+        &matcher
+    )?;
+
+    Ok(())
+}
+
+#[test]
+fn regex_matching_tests() -> Result<(), Box<dyn Error>> {
+    let matcher = setup_matcher()?;
+
+    assert_match("foo", "foo", vec![], &matcher)?;
+    assert_match("{pre command ask}? foo", "foo", vec![], &matcher)?;
+    assert_match("{pre command ask}? foo", "could you foo", vec![], &matcher)?;
+    assert_match("{pre command ask}? foo", "could you please foo", vec![], &matcher)?;
+    assert_match(
+        "{pre command ask}? play [song] on Spotify", 
+        "could you play enter sandman on spotify", 
+        vec![("song", "enter sandman")],
         &matcher
     )?;
 
@@ -28,4 +43,34 @@ fn assert_regex(input_template: &str, expected_regex: &str, matcher: &TemplateMa
     let regex = matcher.convert_template_to_regex(&template)?;
     assert_eq!(expected_regex, regex.as_str());
     Ok(())
+}
+
+fn assert_match(input: &str, statement: &str, bindings: Vec<(&str, &str)>, matcher: &TemplateMatcher) -> Result<(), Box<dyn Error>> {
+    let template = TemplateParser::parse_template(input)?;
+    let matched = matcher.try_match(statement, &template)?;
+    assert_eq!(bindings.len(), matched.num_bindings());
+    for b in bindings {
+        let bound = matched.get_binding(b.0).unwrap();
+        assert_eq!(b.1, bound);
+    }
+    Ok(())
+}
+
+fn assert_err(input_template: &str, input_statement: &str, flavor: TemplateError, matcher: &TemplateMatcher) -> Result<(), Box<dyn Error>> {
+    let template = TemplateParser::parse_template(input_template)?;
+    let matched = matcher.try_match(input_statement, &template);
+    if let Err(error) = matched {
+        assert_eq!(flavor, error);
+        Ok(())
+    } else {
+        panic!("Template match did not produce error (template '{}', input '{}')", input_template, input_statement);
+    }
+}
+
+fn setup_matcher() -> Result<TemplateMatcher, Box<dyn Error>> {
+    let mut matcher = TemplateMatcher::new();
+
+    let pre_command_ask = TemplateParser::parse_template("(could|would) you please?")?;
+    matcher.add_subtemplate("pre command ask", pre_command_ask);
+    Ok(matcher)
 }
