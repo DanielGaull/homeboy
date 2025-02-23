4911c8b090dd76b8ae39298d23e0a41d54586566
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-02-23 12:37:13 -0500

    Added run loop for command-line testing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 use dotenv::dotenv;
-use std::env;
+use homeboy::runner::runner::CommandRunner;
+use std::{env, error::Error, io::{stdin, stdout, Write}};
 
 mod templating;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     let _vars = env::vars();
 
-    println!("Hello, world!");
+    let mut runner = CommandRunner::new();
+    runner.init("./templates.txt")?;
+    loop {
+        print!("Input: ");
+        let line = read_line();
+        runner.run(&line)?;
+    }
+    // Ok(())
+}
+
+fn read_line() -> String {
+    let mut s = String::new();
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    s
 }
